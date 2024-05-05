3453b238c48a2d691863fde3222a8c7a9562b265
Matthew Bixby <matthewbixby@matthews-air.myfiosgateway.com>
2024-05-05 16:37:38 -0400

    more sections added to card content - building out the page to see what it will look like when full

--- src/client/components/Home.jsx
+++ src/client/components/Home.jsx
@@ -15,6 +15,24 @@ function Home() {
           <Typography variant="body2" color="textSecondary" component="p">
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
           </Typography>
+          <Typography variant="h5" component="div">
+            Places of Study
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          </Typography>
+          <Typography variant="h5" component="div">
+            Technologies Mastered
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          </Typography>
+          <Typography variant="h5" component="div">
+            Hobbies and Interests
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          </Typography>
         </CardContent>
       </Paper>
     </div>
