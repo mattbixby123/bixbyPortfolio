da91ad9a294eb51e206ebc27002bb5aa78cce8d7
Matthew Bixby <matthewbixby@matthews-air.myfiosgateway.com>
2024-05-06 15:51:56 -0400

    Contact form successfully sends email with emailjs and timeout used to redurect home

--- src/client/components/Contact.jsx
+++ src/client/components/Contact.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Paper from '@mui/material/Paper';
 import { styled } from '@mui/material/styles';
+import emailjs from '@emailjs/browser'
 
 const ContactPaper = styled(Paper)(({ theme }) => ({
   backgroundColor: '#d9d9d9',
@@ -38,32 +39,64 @@ const FormContainer = styled('form')({
 });
 
 function Contact() {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  
+    try {
+      const formData = {
+        name: e.target.name.value,
+        number: e.target.number.value,
+        email: e.target.email.value,
+        message: e.target.message.value
+      };
+  
+      const response = await emailjs.send('service_d4ree3y', 'template_44npnk9', formData, 'zzimTjXZcCaKXqd7a');
+      console.log('Email sent successfully:', response);
+
+          // Display success message
+      const successMessage = document.createElement('p');
+      successMessage.textContent = 'Email sent successfully!';
+      document.getElementById('contact-form').appendChild(successMessage);
+          // Delay for a moment before redirecting
+      setTimeout(() => {
+        // Navigate to the home page
+        window.location.href = '/';
+      }, 2000); // 2000 milliseconds (2 seconds) delay before redirecting
+
+      // Optionally, display a success message or redirect the user
+    } catch (error) {
+      console.error('Error sending email:', error);
+      // Optionally, display an error message
+    }
+  };
+  
+
   return (
     <div className="ContactForm">
-    <ContactPaper elevation={3}>
-      <section>
-        <p className="main-text-contact">Please feel free to reach out with any questions, comments, or opportunities that you think align with my portfolio, experiences, or interests.</p>
-        <FormContainer>
+      <ContactPaper elevation={3}>
+        <section>
+          <p className="main-text-contact">Please feel free to reach out with any questions, comments, or opportunities that you think align with my portfolio, experiences, or interests.</p>
+          <FormContainer id='contact-form'onSubmit={handleSubmit}>
 
-          <div className="input-group">
-            <input type="text" id="name" required placeholder="Your Name" />
-          </div>
+            <div className="input-group">
+              <input type="text" id="name" required placeholder="Your Name" />
+            </div>
 
-          <div className="input-group">
-            <input type="text" id="number" required placeholder="Phone No." />
-          </div>
+            <div className="input-group">
+              <input type="text" id="number" required placeholder="Phone No." />
+            </div>
 
-          <div className="input-group">
-            <input type="email" id="email" required placeholder="Email" />
-          </div>
+            <div className="input-group">
+              <input type="email" id="email" required placeholder="YourEmail@example.com" />
+            </div>
 
-          <div className="input-group">
-            <textarea id="message" rows="8" placeholder="Your Message"></textarea>
-          </div>
-          <button type="submit">SUBMIT</button>
-        </FormContainer>
-      </section>
-    </ContactPaper>
+            <div className="input-group">
+              <textarea id="message" rows="8" placeholder="Your Message"></textarea>
+            </div>
+            <button type="submit">SUBMIT</button>
+          </FormContainer>
+        </section>
+      </ContactPaper>
     </div>
   );
 }
