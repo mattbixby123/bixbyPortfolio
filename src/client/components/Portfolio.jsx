01251baf2cf58d96673a6cd72e7d3d2d4603ec57
mattbixby123 <matthew.bixby1@gmail.com>
2024-09-26 14:18:58 -0400

    reverse project order implimented to showcase newer code on portfolio page

--- src/client/components/Portfolio.jsx
+++ src/client/components/Portfolio.jsx
@@ -4,7 +4,7 @@ import ProjectCard from './ProjectCard';
 
 const projects = [
   {
-    title: "#1 Puppy Bowl",
+    title: "Puppy Bowl",
     emoji: "🐶",
     description: "This application is for managing a Puppy Bowl competition. Users will be able to view a list of players competing in the bowl, access detailed information about each player, create new players through a form, search for specific players, and delete players they have created.",
     technologies: ["React-Redux", "React Router", "Redux Toolkit", "HTML5", "CSS", "JavaScript", "JavaScript APIs"],
@@ -12,7 +12,7 @@ const projects = [
     code: "https://github.com/mattbixby123/PuppyBowlSPA"
   },
   {
-    title: "#2 Book Buddy",
+    title: "Book Buddy",
     emoji: "📗",
     description: "This project is a web app for a fictitious library. This app allows you to browse a category of books that are fetched from an external API. As a registered user, you can checkout books and return them. You can also view your reserved books when accessing the account route.",
     technologies: ["React", "React Router", "React Hooks", "Redux Toolkit", "RTK Query", "Authentication", "HTML5", "CSS", "JavaSript"],
@@ -20,7 +20,7 @@ const projects = [
     code: "https://github.com/mattbixby123/B30A-BookBuddy-Bixby"
   },
   {
-    title: "#3 Classroom Manager",
+    title: "Classroom Manager",
     emoji: "🧑‍🏫",
     description: "This Classroom Manager incorporates Prisma and Prisma Client, creating a Prisma schema to define instructor and student models, and developing the seed.js file to work seamlessly with Prisma Migrate. User passwords are encrypted using bcrypt before being transmitted to the server. Users have the option to register or log in with GitHub via OAuth to access the site.",
     technologies: ["React-Redux", "React-Router", "Redux Toolkit", "Express JS", "PostgreSQL", "Prisma ORM", "bcrypt encryption", "GitHub OAuth"],
@@ -28,7 +28,7 @@ const projects = [
     code: "https://github.com/mattbixby123/Unit4.ClassroomManager.Starter"
   },
   {
-    title: "#4 Full Stack eCommerce Application",
+    title: "Full Stack eCommerce Application",
     emoji: "💸",
     description: "The capstone project is an e-commerce platform for buying vintage paper media (books, comics, and magazines). It offers customers and admins a wide array of features and tools. The project is meticulously crafted for scalability, performance, and user satisfaction. Emphasizing meticulous database management, robust security measures, and seamless integration with payment gateways, the project aims to deliver a cutting-edge e-commerce solution tailored to meet diverse user needs.",
     technologies: ["React-Redux", "React-Router", "Redux Toolkit", "Express JS", "PostgreSQL", "Prisma ORM", "bcrypt encryption", "MUI Styling & Pagination", "STRIPE payment processing"],
@@ -36,7 +36,7 @@ const projects = [
     code: "https://github.com/mattbixby123/eCommCapstone"
   },
   {
-    title: "#5 moodTrak",
+    title: "moodTrak",
     emoji: "📆",
     description: "How do you feel today? This is a daily mood tracking app that was completed in a single weekend. This app features a landing page to login/register as well as a dashboard page to log your mood and see your past records.",
     technologies: ["React", "NextJS", "Tailwind CSS", "Firebase", "Netlify deployment"],
@@ -44,7 +44,7 @@ const projects = [
     code: "https://github.com/mattbixby123/moodtrak"
   },
   {
-    title: "#6 Horizon Bank Acct. Manager",
+    title: "Horizon Bank Acct. Manager",
     emoji: "🏦",
     description: "How do you feel today? This is a daily mood tracking app that was completed in a single weekend. This app features a landing page to login/register as well as a dashboard page to log your mood and see your past records.",
     technologies: ["React", "NextJS", "TypeScript", "Tailwind CSS", "Appwrite"],
@@ -62,7 +62,7 @@ function Portfolio() {
         </Typography>
       </Paper> */}
       <Grid container spacing={3}>
-        {projects.map((project, index) => (
+        {projects.slice().reverse().map((project, index) => (
           <Grid item xs={12} sm={6} md={6} lg={3} key={index}>
             <ProjectCard {...project} />
           </Grid>
